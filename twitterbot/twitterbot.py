768ed9daea7fa3ca9ea28807c51fc0b52cbb42cf
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-03-25 11:55:43 -0400

    Basic streaming bot

--- twitterbot/twitterbot.py
+++ twitterbot/twitterbot.py
@@ -8,4 +8,26 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-api.update_status("Hello!")
+#api.update_status("Hello!")
+
+def get_award(name):
+    pass
+
+def get_name_from_tweet(tweet):
+    pass
+
+def reply(award):
+    api.update_status(award)
+
+
+class MLBListener(tweepy.StreamListener):
+    
+    def on_status(self, status):
+        if "@bestinmlb" in status.text:
+            print(status.text)
+
+
+if __name__ == "__main__":
+    listener = MLBListener()
+    stream = tweepy.Stream(auth=api.auth, listener=listener)
+    stream.userstream()
