79110b8cc41e0854d46564423a44030bf4b5bd6b
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-03-25 11:10:41 -0400

    Hello world twitterbot

---
+++ twitterbot/twitterbot.py
@@ -0,0 +1,11 @@
+# Import our Twitter credentials from credentials.py
+import tweepy
+from time import sleep
+from credentials import *
+
+# Access and authorize our Twitter credentials from credentials.py
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+api.update_status("Hello!")
