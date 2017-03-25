3682ded2d7935a65bb2ed1d1e8891d0d78982247
Nathan Smith <nathan.smith5@mail.mcgill.ca>
2017-03-25 11:02:46 -0400

    twitterbot credentials

---
+++ twitterbot/credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'RCdiLDcM9ldGmDFOgJaR299wz'
+consumer_secret = 'g3MgewUMWzIEdpCP5snOJiAuotXiqXwjJEUfCZXz33l20nYJDW'
+access_token = '845647386517143552-s8RoAhAuTOQnOrMYIfczQRAUCPhT8LU'
+access_token_secret = 'oujXKCGI74BcsQ9hsEw5NQMZkCPnBm6Yv84VLBHsqzPr9'
